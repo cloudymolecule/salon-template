fe6e7c5a907b5fa6147616eaf9a85d92c454e222
Cloudymolecule <baires82@gmail.com>
2021-08-02 15:46:12 -0400

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,5 @@
-import React from 'react'
-import { IconContext } from 'react-icons/lib'
 import React, { useState } from 'react'
+import { IconContext } from 'react-icons/lib'
 import { FaBars, FaTimes } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import '../styles/Nav.css'
@@ -16,10 +15,38 @@ function Nav() {
     const closeMobileMenu = () => {
         setClick(false)
     }
-    
+
     return (
         <>
-
+            <IconContext.Provider value={{ color: '#fff' }}>
+                <div className='navbar'>
+                    <div className='navbar-container container'>
+                        <Link onClick={closeMobileMenu} to='/' className='navbar-logo'>
+                            <div className='navbar-name'>Salon Name</div>
+                        </Link>
+                        <div onClick={handleClick} className='menu-icon'>
+                            {click ? <FaTimes /> : <FaBars />}
+                        </div>
+                        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                            <li className='nav-item'>
+                                <Link onClick={closeMobileMenu} className='nav-link' to='/'>HOME</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link onClick={closeMobileMenu} className='nav-link' to='/services'>SERVICES</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link onClick={closeMobileMenu} className='nav-link' to='/lookbook'>LOOKBOOK</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link onClick={closeMobileMenu} className='nav-link' to='/contact'>CONTACT</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link onClick={closeMobileMenu} className='nav-link' to='/our-staff'>OUR STAFF</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </IconContext.Provider>
         </>
     )
 }
