6e84b3ef823269207ff62e4f0f460b1f1dae3331
Cloudymolecule <baires82@gmail.com>
2021-08-02 15:32:51 -0400

    Add routes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,27 @@
+import  { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import '../styles/App.css'
+import Home from '../components/Home'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+import LookBook from '../components/LookBook'
+import Nav from '../components/Nav'
+import Products from '../components/Products'
+import Services from '../components/Services'
+import Staff from '../components/Staff'
 
 function App() {
   return (
-    <div>
-      <h1>APP.JS</h1>
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' exact component={Services} />
+        <Route path='/lookbook' exact component={LookBook} />
+        <Route path='/products' exact component={Products} />
+        <Route path='/our-staff' exact component={Staff} />
+        <Route path='/contact' exact component={Contact} />
+      </Switch>
+    </Router>
   )
 }
 
