39750caca09edc08f7959c8308d994273c152b1d
Cloudymolecule <baires82@gmail.com>
2021-08-05 09:15:49 -0400

    Add Amenities components and css files

---
+++ src/components/home/Amenities.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../styles/home/Amenities.css'
+
+function Amenities() {
+    return (
+        <div className='amenities'>
+            <div>Amenities</div>
+        </div>
+    )
+}
+
+export default Amenities
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import '../styles/home/Home.css'
 import Banner from '../components/home/Banner'
 import WhatsNew from '../components/home/WhatsNew'
+import Amenities from '../components/home/Amenities'
 
 function Home() {
     return (
         <div>
             <Banner />
             <WhatsNew />
+            <Amenities />
         </div>
     )
 }
