0ec19f3128856c34f86bc37c1022c0550c3cd6f4
Cloudymolecule <baires82@gmail.com>
2021-08-05 09:33:50 -0400

    Update Amenities.js

--- src/components/home/Amenities.js
+++ src/components/home/Amenities.js
@@ -4,7 +4,39 @@ import '../../styles/home/Amenities.css'
 function Amenities() {
     return (
         <div className='amenities'>
-            <div>Amenities</div>
+            <div className='amenities-welcome'>
+                <div className='amenities-welcome-title'>Welcome to our Salon!</div>
+                <div className='amenities-welcome-desc'>
+                    Cupidatat dolor culpa eiusmod labore voluptate cillum ad magna labore laboris ea deserunt. Ex occaecat officia proident dolore laborum deserunt aliqua esse magna ullamco enim labore anim magna. Dolor exercitation veniam sunt exercitation officia minim nulla sunt quis labore. Adipisicing commodo non pariatur qui et irure irure ad velit excepteur in commodo enim. Id veniam nostrud anim deserunt commodo minim aliqua eiusmod labore non officia. Aliquip Lorem in anim amet incididunt sint elit quis occaecat.
+                </div>
+            </div>
+            <div className='amenities-amenities'>
+                
+                <div className='amenities-1'>
+                    <div className='amenities-1-title'>Hair</div>
+                    <div className='amenities-1-desc'>
+                        Culpa ex dolor anim deserunt cillum excepteur excepteur do mollit eu deserunt consectetur veniam deserunt. Ipsum proident est irure sunt incididunt amet aliqua sint dolore adipisicing. Reprehenderit voluptate irure aute reprehenderit commodo magna culpa velit laboris nulla officia enim consectetur laborum. Dolore deserunt laborum voluptate do incididunt cupidatat fugiat. Ad velit ea officia fugiat deserunt dolore commodo do amet non.
+                    </div>
+                    <div className='amenities-1-img'>image</div>
+                </div>
+
+                <div className='amenities-1'>
+                    <div className='amenities-1-title'>Spa & Waxing</div>
+                    <div className='amenities-1-desc'>
+                        Eu quis enim minim exercitation velit. Qui duis ipsum ipsum dolor eiusmod. In fugiat sunt quis laborum labore aliqua nostrud. Aliquip quis veniam in consequat quis dolor duis laboris eiusmod. Et cillum sunt pariatur excepteur anim excepteur excepteur excepteur excepteur amet. Ipsum qui id nisi quis Lorem proident ad sint elit aliquip tempor. Ipsum cupidatat laboris dolor incididunt.
+                    </div>
+                    <div className='amenities-1-img'>image</div>
+                </div>
+
+                <div className='amenities-1'>
+                    <div className='amenities-1-title'>Nails</div>
+                    <div className='amenities-1-desc'>
+                        Occaecat proident officia pariatur in. Incididunt nostrud dolor et velit qui nisi sit id tempor reprehenderit. Commodo ipsum enim laborum eiusmod voluptate culpa.
+                    </div>
+                    <div className='amenities-1-img'>image</div>
+                </div>
+                
+            </div>
         </div>
     )
 }
