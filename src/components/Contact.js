ff7f0e1657eb1e434b4071ee39a9e5ef757631fb
Cloudymolecule <baires82@gmail.com>
2021-08-06 07:00:04 -0400

    Add react icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import '../styles/Contact.css'
+import { IconContext } from 'react-icons/lib'
+import { AiFillFacebook, AiFillInstagram } from 'react-icons/ai'
+import { MdEmail } from 'react-icons/md'
+import { SiGooglemaps } from 'react-icons/si'
+
 
 function Contact() {
     return (
-        <div>
-            <p>Contact</p>
+        <div className='contact-container'>
+            <IconContext.Provider value={{ color: '#00eeff', size: '80px' }}>
+                <a className='contact-item' href='https://www.facebook.com/' target="_blank" rel="noreferrer">
+                    <AiFillFacebook />
+                </a>
+                <a className='contact-item' href='https://www.instagram.com/user/' target="_blank" rel="noreferrer">
+                    <AiFillInstagram />
+                </a>
+                <a className='contact-item' href='mailto: email' target="_blank" rel="noreferrer">
+                    <MdEmail />
+                </a>
+                <a className='contact-item' href='https://www.maps.google.com/' target="_blank" rel="noreferrer">
+                    <SiGooglemaps />
+                </a>
+            </IconContext.Provider>
+
         </div>
     )
 }
