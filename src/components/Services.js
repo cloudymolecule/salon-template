6e7b564b4a26990a0836dd7e1b285df042a6b30c
Cloudymolecule <baires82@gmail.com>
2021-08-10 11:53:30 -0400

    Add services

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,10 +2,18 @@ import React from 'react'
 import '../styles/Services.css'
 
 
+
 function Services() {
     return (
-        <div>
-            <p>Services</p>
+        <div className='services-container'>
+            <span>Our Services</span>
+            <div className='services'>
+                <div className='service'>
+                    <
+                </div>
+                
+
+            </div>
         </div>
     )
 }
---
+++ src/constants/Services.js
@@ -0,0 +1,65 @@
+export const Services = [
+    {
+        title: 'CUT & STYLING',
+        description: 'Precision Cutting, Beautiful Styling',
+        services: [
+            {service: 'Shampoo, Cut, & Style', price: '$55-80'},
+            {service: 'Shampoo & Style', price: '$40+'},
+            {service: "Gentleman's Haircut", price: '$25-50'},
+            {service: '​Deva Cut', price: '$65-100'},          
+            {service: 'Kiddie Cut (ages 10 and under)', price: '$25+'},
+            {service: 'Special Evening Style', price: '$75+'},
+            {service: 'Fringe Trim', price: '$15'},
+            {service: 'Updo', price: '$85+'},
+            {service: 'Deep Conditioning', price: '$30+'}
+        ]
+    },
+    {
+        title: 'BLOW OUT BAR',
+        description: 'Big Sexy or Sleek Blowouts',
+        services: [
+            {service: 'Classic', price: '$55-80'},
+            {service: 'Big Sexy', price: '$40+'},
+            {service: "Curly", price: '$25-50'},
+            {service: '​Curly to Straight', price: '$65-100'},          
+            {service: 'Packages:', price: '6 Blowouts $200, 12 Blowouts $380'}
+        ]
+    },
+    {
+        title: 'COLORING',
+        description: 'Coloring and Highlighting',
+        services: [
+            {service: 'Single Process Color', price: '$65+'},
+            {service: 'Double Process Color', price: '$75+'},
+            {service: "New Color", price: '$75+'},
+            {service: 'Partial Highlight', price: '$90-125'},          
+            {service: 'Full Highlights', price: '$125+'},
+            {service: 'PM Shine Color', price: '$40+'},
+            {service: 'Corrective Colo', price: 'Consultation Required'}
+        ]
+    },
+    {
+        title: 'NAIL SERVICES',
+        description: '',
+        services: [
+            {service: 'Manicure', price: '$20'},
+            {service: 'French Manicure', price: '$25'},
+            {service: "Pedicure", price: '$45'},
+            {service: 'Gel Manicure', price: '$45'},          
+            {service: 'Polish Change', price: '$15'},
+            {service: 'Buff & Seal', price: '$30'},
+            {service: 'LCN Overlay', price: '$80'},
+            {service: 'LCN Maintenance', price: '$50'}
+        ]
+    },
+    {
+        title: 'MAKEUP',
+        description: '',
+        services: [
+            {service: 'Makeup Application', price: '$85+'},
+            {service: 'Makeup Application with Instructions', price: '$100+'}
+        ]
+    }
+
+]
+
