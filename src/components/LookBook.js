59357460c5d5e59f885a407a25ad7eb35781ef58
Cloudymolecule <baires82@gmail.com>
2021-08-13 10:23:06 -0400

    Add Lightbox to gallery

--- src/components/LookBook.js
+++ src/components/LookBook.js
@@ -1,17 +1,42 @@
-import '../styles/LookBook.css'
 import { LookbookSrc } from '../constants/LookbookSrc' 
-import Gallery from "react-photo-gallery";
-
+import Gallery from "react-photo-gallery"
+import React, { useState, useCallback } from "react"
+import Carousel, { Modal, ModalGateway } from "react-images"
+import '../styles/LookBook.css'
 
 function LookBook() {
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false)
+
+    const openLightbox = useCallback((event, { photo, index }) => {
+        setCurrentImage(index)
+        setViewerIsOpen(true)
+    }, [])
+
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setViewerIsOpen(false)
+    }
     return (
         <>
             <div className='lookbook-container'>
-                <Gallery photos={LookbookSrc} direction={"column"} />
-                
+                <Gallery photos={LookbookSrc} onClick={openLightbox} />
+                <ModalGateway>
+                    {viewerIsOpen ? (
+                    <Modal onClose={closeLightbox}>
+                        <Carousel
+                        currentIndex={currentImage}
+                        views={LookbookSrc.map(x => ({
+                            ...x,
+                            srcset: x.srcSet,
+                            caption: x.title
+                        }))}
+                        />
+                    </Modal>
+                    ) : null}
+                </ModalGateway>
             </div>
         </>
-        
     )
 }
 
--- src/constants/LookbookSrc.js
+++ src/constants/LookbookSrc.js
@@ -1,41 +1,81 @@
 export const LookbookSrc = [
     {
-        src: 'https://picsum.photos/200/300',
+        src: 'https://picsum.photos/200/302',
         width: 4,
         height: 3
     },
     {
-        src: 'https://picsum.photos/200/300',
+        src: 'https://picsum.photos/200/303',
         width: 1,
         height: 1
     },
     {
-        src: 'https://picsum.photos/200/300',
+        src: 'https://picsum.photos/200/301',
         width: 4,
         height: 3
     },
     {
-        src: 'https://picsum.photos/200/300',
+        src: 'https://picsum.photos/200/304',
         width: 1,
         height: 1
     },
     {
-        src: 'https://picsum.photos/200/300',
+        src: 'https://picsum.photos/200/305',
         width: 4,
         height: 3
     },
     {
-        src: 'https://picsum.photos/200/300',
+        src: 'https://picsum.photos/200/306',
         width: 1,
         height: 1
     },
     {
-        src: 'https://picsum.photos/200/300',
+        src: 'https://picsum.photos/200/307',
         width: 4,
         height: 3
     },
     {
-        src: 'https://picsum.photos/200/300',
+        src: 'https://picsum.photos/200/308',
+        width: 1,
+        height: 1
+    },
+    {
+        src: 'https://picsum.photos/200/309',
+        width: 4,
+        height: 3
+    },
+    {
+        src: 'https://picsum.photos/200/310',
+        width: 1,
+        height: 1
+    },
+    {
+        src: 'https://picsum.photos/200/311',
+        width: 4,
+        height: 3
+    },
+    {
+        src: 'https://picsum.photos/200/312',
+        width: 1,
+        height: 1
+    },
+    {
+        src: 'https://picsum.photos/200/313',
+        width: 4,
+        height: 3
+    },
+    {
+        src: 'https://picsum.photos/200/314',
+        width: 1,
+        height: 1
+    },
+    {
+        src: 'https://picsum.photos/200/315',
+        width: 4,
+        height: 3
+    },
+    {
+        src: 'https://picsum.photos/200/316',
         width: 1,
         height: 1
     }
