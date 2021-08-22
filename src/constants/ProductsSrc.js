e57cebfd4ee9f98f2886bf97fa26ed179b75eebd
Cloudymolecule <baires82@gmail.com>
2021-08-22 12:27:27 -0400

    Create ProductsSrc.js

---
+++ src/constants/ProductsSrc.js
@@ -0,0 +1,42 @@
+export const ProductsSrc = [
+    {
+        name: 'Arrojo',
+        nameColor: 'fff',
+        description: 'Our hottest new line of products from NYC based stylist Nick Arrojo. Express Yourself stylists loved his cutting and color classes so much they brought the NYC line to Far Hills. We carry the whole product line which contains everything from styling products, shampoos, and deep conditioners.'
+    },
+    {
+        name: 'Keratin Complex',
+        nameColor: 'fff',
+        description: "Keratin Complex – the original, in-salon service that infuses natural keratin deep into hair's cuticle. This revolutionary therapy eliminates up to 95% of frizz and curl and instantly adds amazing shine, silkiness and condition to the hair. The results are astounding. Clients rave that 'this is a life-changing experience'. Hair is more manageable, smoother and easier to style than ever before. It is perfect on all types of hair – including color treated and chemically processed hair!"
+    },
+    {
+        name: 'Moroccan Oil',
+        nameColor: 'fff',
+        description: 'As the pioneer of oil-infused hair care, our original Moroccanoil Treatment created the worldwide buzz on argan oil and paved the way for an extensive line of oil-infused hair care products to address the needs of all hair types. Moroccanoil offers innovative, easy-to-use, performance-driven formulas that dramatically transform all hair types resulting in beautiful, healthy, natural-looking hair.'
+    },
+    {
+        name: 'DevaCurl',
+        nameColor: 'fff',
+        description: 'All DevaCurl products are 100% sulfate, silicone and paraben free, but there’s more. The entire line is inspired by conversations with real curly girls. Our innovative formulas are made to fit into your daily routine. From our cleansers and conditioners that hydrate your hair in the shower to our easy styling products that work on wet or dry curls, it all adds up to endless amazing hair days.'
+    },
+    {
+        name: 'Organic Systems',
+        nameColor: 'fff',
+        description: 'Contains some of the best natural ingredients to produce a gentle color which gives a superb result every time.  Antioxidants prevent and repair damage done to the body by these free radicals. Organic Color Systems includes the natural antioxidants certified organic orange (Citrus Dulcis) and grapefruit (Citrus Grandis), as well as vitamins C and E. The conditioning and moisturizing function is fulfilled by Hydrolyzed Wheat Protein, which also helps to strengthen hair.'
+    },
+    {
+        name: 'Goldwell',
+        nameColor: 'fff',
+        description: 'We think stylist. So we know that only a stylist-exclusive brand that meets all your professional needs can help you reach your goals. A full portfolio for stunning results. Technical advances that streamline your work. Service concepts that make your clients happy.'
+    },
+    {
+        name: 'Tocco Magico',
+        nameColor: 'fff',
+        description: 'Italian Organic Hair color and Hair treatment brand name TOCCO MAGICO has been introduced in UAE. Established in 1938 the company has been producing hair color and treatment product which takes care of all needs professional hair colorists and Stylists.'
+    },
+    {
+        name: 'YonKa',
+        nameColor: 'fff',
+        description: 'Discover what French women have been using for over 50 years to fight wrinkles. A professional skincare line carried in spas worldwide, Yon-Ka features botanically-enriched creams, lotions and concentrates, all tailored to help meet every need from extremely sensitive skin to aging, sagging, acne, dehydration, hyperpigmentation, and even post-surgery skin. Long favored by celebrities and spa connoisseurs, YonKa continues to be featured in top magazines and newspapers and is the top choice for makeup sets of films and TV shows.'
+    }
+]
