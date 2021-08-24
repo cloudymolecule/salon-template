e4c672fcf471dc2d283994e004bc38e1fa2f6007
Cloudymolecule <baires82@gmail.com>
2021-08-24 09:42:18 -0400

    Add product iteration

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import '../styles/Products.css'
+import { ProductsSrc } from '../constants/ProductsSrc'
 
 
 function Products() {
-    return (
-        <div>
-            <p>Products</p>
+    
+    const prods = ProductsSrc.map(p => {
+        return <div>
+            <h2 className='product-name'>{p.name}</h2>
+            <p className='product-desc'>{p.description}</p>
         </div>
+    })
+
+    return (
+        <>
+            {prods}
+        </>
     )
 }
 
